40cd7d54bbcdc700755858c7bd2c7ccbad1fd460
guidelli <agat10680@gmail.com>
2023-12-12 16:19:31 +0100

    c

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,25 @@ import { Effect, Console } from 'effect';
 const program = Console.log('Hello, World!');
 
 Effect.runSync(program);
+
+
+
+const p = Effect.succeed(42)
+
+console.log(Effect.runSync(p));
+
+
+const divide = (a: number, b: number): Effect.Effect<never, Error, number> =>
+  b === 0
+    ? Effect.fail(new Error("Cannot divide by zero"))
+    : Effect.succeed(a / b)
+
+
+console.log((()=>{
+    try {
+        Effect.runSync(divide(3,0))
+        
+    } catch (error) {
+        return error
+    }
+    })())
