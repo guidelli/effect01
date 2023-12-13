134956154accd532b40b7364b38e01d196033276
guidelli <agat10680@gmail.com>
2023-12-13 11:08:29 +0100

    c

--- src/index.ts
+++ src/index.ts
@@ -21,14 +21,39 @@ console.log((()=>{
     try {
         Effect.runSync(divide(3,0))
         
-    } catch (error) {
-        return error
+    } catch (error: any) {
+       return error.message
     }
     })())
 
 
 // $ExpectType Effect<never, never, number>
-const program = Effect.sync(() => {
+const  p2 = Effect.sync(() => {
     console.log("Hello, World!") // side effect
     return 42 // return value
   })
+
+Effect.runSync(p2);
+
+// $ExpectType Effect<never, UnknownException, any>
+const p3 = Effect.try(
+    () => JSON.parse("") // JSON.parse may throw for bad input
+)
+
+
+console.log((()=>{
+    try {
+        Effect.runSync(p3)
+    } catch (error: any) {
+        return error.message
+    }
+})())
+
+
+const p4= Effect.try({
+    try: ()=>JSON.parse(''),
+    catch: unknown=>new Error(`something went wrong ${unknown}`)
+})
+
+
+Effect.runSync(p4);
